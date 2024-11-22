4c22945da85e919b45ff4dc9535c1c162d6db62f
Vardhan077 <132035215+Vardhan077@users.noreply.github.com>
2024-11-22 13:43:56 +0530

    Update index.js

--- Mini-Loan-App-main/server/index.js
+++ Mini-Loan-App-main/server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://mini-loan-app-inky.vercel.app/","http://localhost:3000","mongodb://localhost:27017/"],
+    origin: ["https://mini-loan-app-inky.vercel.app/","http://localhost:3000","mongodb://localhost:27017/","https://mini-loan-webapp.vercel.app/"],
     methods: ["POST", "GET","PUT","DELETE"],
     credentials: true
   }
